92496e99560ff54757e90da54bba654b3b6460af
apollolongship <132106900+keepflowing@users.noreply.github.com>
2025-03-31 21:50:53 -0500

    Add url checking for Bridge

--- btccore-bridge/src/lib.rs
+++ btccore-bridge/src/lib.rs
@@ -1,7 +1,8 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use url::Url;
 
 pub trait ZmqReceiver {
-    fn recv(&self) -> Result<Vec<u8>, String>;
+    fn recv(&self) -> Result<Vec<u8>>;
 }
 
 pub struct Bridge<T: ZmqReceiver> {
@@ -14,6 +15,27 @@ pub struct Bridge<T: ZmqReceiver> {
 
 impl<T: ZmqReceiver> Bridge<T> {
     pub fn new(rpc_address: &str, zmq_address: &str, zmq_receiver: T) -> Result<Self> {
+        // Parsing urls and doing additional checks
+        let rpc_url = Url::parse(rpc_address).context("Invalid RPC address.")?;
+        if !["http", "https"].contains(&rpc_url.scheme()) {
+            return Err(anyhow!("Only http or https are allowed for RPC."));
+        }
+
+        let zmq_url = Url::parse(zmq_address).context("Invalid ZMQ address.")?;
+        match zmq_url.scheme() {
+            "tcp" => {
+                if zmq_url.host().is_none() || zmq_url.port().is_none() {
+                    return Err(anyhow!("zmq tcp address must have host and port"));
+                }
+            }
+            "ipc" => {
+                if zmq_url.path().is_empty() {
+                    return Err(anyhow!("zmq ipc address must have a path"));
+                }
+            }
+            _ => return Err(anyhow!("Only tcp or ipc are allowed for ZMQ.")),
+        }
+
         Ok(Bridge {
             current_header: None,
             last_block_hash: None,
@@ -27,19 +49,61 @@ impl<T: ZmqReceiver> Bridge<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    //use anyhow::Result;
+
     struct MockReceiver;
     impl ZmqReceiver for MockReceiver {
-        fn recv(&self) -> Result<Vec<u8>, String> {
+        fn recv(&self) -> anyhow::Result<Vec<u8>> {
             Ok(vec![1, 2, 3])
         }
     }
 
     #[test]
-    fn bridge_is_created() {
+    fn init_bridge_valid_urls() {
         let mock_receiver = MockReceiver;
-        let bridge = Bridge::new("1111", "1111", mock_receiver).unwrap();
+        let bridge = Bridge::new(
+            "http://localhost:8332",
+            "tcp://127.0.0.1:28332",
+            mock_receiver,
+        );
+        assert!(bridge.is_ok());
 
-        assert_eq!(bridge.last_block_hash, None);
-        assert_eq!(bridge.current_header, None);
+        let mock_receiver = MockReceiver;
+        let bridge = Bridge::new(
+            "https://minecraft.net:8332",
+            "ipc:///tmp/zmq.sock",
+            mock_receiver,
+        );
+        assert!(bridge.is_ok());
+    }
+
+    #[test]
+    fn init_bridge_invalid_rpc_address_fails() {
+        let mock_receiver = MockReceiver;
+        let bridge = Bridge::new(
+            "ftp://localhost:8332",
+            "tcp://127.0.0.1:28332",
+            mock_receiver,
+        );
+
+        assert!(bridge.is_err());
+
+        let mock_receiver = MockReceiver;
+        let bridge = Bridge::new("bad_url", "tcp://127.0.0.1:28332", mock_receiver);
+
+        assert!(bridge.is_err());
+    }
+
+    #[test]
+    fn init_bridge_invalid_zmq_address_fails() {
+        let mock_receiver = MockReceiver;
+        let bridge = Bridge::new("http://localhost:8332", "tcp://", mock_receiver);
+
+        assert!(bridge.is_err());
+
+        let mock_receiver = MockReceiver;
+        let bridge = Bridge::new("https://localhost:8332", "my_address", mock_receiver);
+
+        assert!(bridge.is_err());
     }
 }
